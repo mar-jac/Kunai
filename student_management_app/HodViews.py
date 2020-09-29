085400f48c657d129113622ab964db975e0d96b7
mar-jac <mar.jacq0296@gmail.com>
2020-09-29 18:57:55 -0400

    Added add_student_template.html and student classes to the urls.py.

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-from student_management_app.models import CustomUser, Staffs, Courses
+from student_management_app.models import CustomUser, Staffs, Students, Courses
 
 
 def admin_home(request):
@@ -51,3 +51,41 @@ def add_course_save(request):
         except:
             messages.error(request, "Failed To Add Course")
             return HttpResponseRedirect("/add_course")
+
+def add_student(request):
+    courses=Courses.objects.all()
+    return render(request,"hod_template/add_student_template.html", {"courses" : courses})
+
+def add_student_save(request):
+    if request.method != "POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        address = request.POST.get("address")
+        session_start=request.POST.get("session_start")
+        session_end=request.POST.get("session_end")
+        course_id=request.POST.get("course")
+        sex=request.POST.get("sex")
+
+        user = CustomUser.objects.create_user(username=username, password=password, email=email,
+                                              last_name=last_name,
+                                              first_name=first_name, user_type=2)
+        user.students.address = address
+        course_obj = Courses.objects.get(id=course_id)
+        user.students.course_id = course_obj
+        user.students.session_start_year = session_start
+        user.students.session_end_year = session_end
+        user.students.gender = sex
+        user.students.profile_pic = ""
+        user.save()
+
+        messages.success(request, "Successfully Added Student")
+        return HttpResponseRedirect("/add_student")
+
+        messages.error(request, "Failed to Add Student to Roster")
+        return HttpResponseRedirect("/add_student")
+
