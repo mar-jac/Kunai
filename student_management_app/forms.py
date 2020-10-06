1b3e98e1a15adddcc516449aa5e45f84b8d9ebd1
mahmudulkalam <mahmudulkalam@yahoo.com>
2020-10-05 23:45:48 -0400

    added calendar functionality again

--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from student_management_app.models import Courses
 
+class DateInput(forms.DateInput):
+    input_type = "date"
 
 class AddStudentForm(forms.Form):
     email=forms.EmailField(label="Email",max_length=50)
@@ -26,6 +28,6 @@ class AddStudentForm(forms.Form):
 
     course=forms.ChoiceField(label="Course",choices=course_list)
     sex=forms.ChoiceField(label="Sex",choices=gender_choice)
-    session_start=forms.DateField(label="Session Start")
-    session_end=forms.DateField(label="Session End")
+    session_start=forms.DateField(label="Session Start",widget=DateInput())
+    session_end=forms.DateField(label="Session End",widget=DateInput())
     profile_pic=forms.FileField(label="Profile Pic",max_length=50)
